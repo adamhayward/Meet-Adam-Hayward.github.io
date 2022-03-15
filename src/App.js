3160ce1ff8c3213f4c8cf01c461025637028fc5f
Adam Hayward <a.hayward11@gmail.com>
2022-03-15 14:14:08 -0500

    renevating site, posting home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Wrapper from "./components/Wrapper";
+
 import Home from "./components/pages/Home";
-import Portfolio from "./components/pages/Portfolio";
-import Contact from "./components/pages/Contact";
-import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
-    <div>
-      <NavTabs />
       <Route exact path="/Meet-Adam-Hayward" component={Home} />
-    <Wrapper>
       <Route exact path="/" component={Home} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
-      </Wrapper>
-      <Footer/>
-    </div>
   </Router>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,52 +1,122 @@
 import React from "react";
-import Container from "../Container";
-import Row from "../row";
-import Col from "../col";
-import Title from "../Title";
+import "./home.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import headShot from "../../images/headshot.png";
+import linkedInLogo from "../../images/linkedin-logo.svg";
+import gitHubLogo from "../../images/github-logo.svg";
+import instagramLogo from "../../images/instagram-logo.svg";
 
 function Home() {
   return (
     <Container>
       <Row>
-        <Col>
-          <Title>about</Title>
-        </Col>
-      </Row>
-      <Row className="row-auto">
-        <Col>
-          <p />
+        <Col id="headShotCol" xs={12} sm={4}>
           <img
+            id="headShotImg"
+            src={headShot}
             className="img-responsive"
-            alt="Adam Hayward's head shot"
-            style={{float: "left", marginRight: "2%", width: "225px"}}
+            alt="Adam Hayward posing in a suit"
           />
-          <span className="text-uppercase" style={{ color: "orange"}}>Hello.</span> &nbsp;
-          My name is Adam Hayward. I recently moved from Dallas, Texas to
-          Anchorage, Alaska. I relocated to Anchorage to be near family, and of
-          course, for adventures in this huge playground that is America's last
-          frontier.
-          <br />
-          Prior to moving, I spent the last 8 years as a manager with AT&T,
-          supervising a team of up to 25 technitians. While I enjoyed my ride at
-          AT&T, I was seeking both a lifestyle change and a career change. I
-          learned from my experiences at AT&T, after being exposed to Lean and
-          Agile methodologies, that something in the Information Management
-          (technology) field would be a good fit for me. After doing a lot of
-          research, I resigned from my position and currently have the luxury of
-          focusing on completing a Full Stack Certificate though SMU.
         </Col>
-      </Row>
-      <Row className="row-auto">
-        <Col>
-          <p>
-            Although I am new to programming, I’ve been told more than once in
-            the last few years, that I was born for problem solving. I am
-            starting to agree as I am enjoying the program emensely, challenges
-            and all. Programmers I know have told me that after working hard –
-            frustration and all – that solving the problems enherent to coding,
-            is a rush you rarlely get day-to-day in other professions. I’m
-            already hooked.
-          </p>
+        <Col
+          id="greetingCol"
+          sm={12}
+          md={8}
+          className="displayFlex justifyContentCenter"
+          style={{ flexDirection: "column" }}
+        >
+          <div id="column2">
+            <Row>
+              <Col>
+                <div className="displayFlex">
+                  <div id="greetingLine1">Hi there,</div>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div className="displayFlex">
+                  <div id="greetingLine2">I'm</div>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div
+                  className="displayFlex justifyContentCenter haywardContainer"
+                  style={{
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <div id="greetingLine3">Adam Hayward</div>
+                  <div id="greetingLine4">Welcome to my portfolio</div>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div id="banner">
+                  <div id="bannerRow1">Exciting Improvements are under way</div>
+                  <div id="bannerRow2">Make sure to check back soon</div>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="displayFlex justifyContentCenter " xs={12} xl={9}>
+                <Button className="btnCol" id="btnResume">
+                  View Resume
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="displayFlex justifyContentCenter" xs={4} xl={3}>
+                <a
+                  href="https://www.linkedin.com/in/adam-hayward-1562b4173/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-responsive icon btnCol"
+                    src={linkedInLogo}
+                    alt="LinkedIn icon"
+                  />
+                </a>
+              </Col>
+              <Col className="displayFlex justifyContentCenter" xs={4} xl={3}>
+                <a href="https://github.com/adamhayward" target="_blank">
+                  <img
+                    className="img-responsive icon btnCol"
+                    src={gitHubLogo}
+                    alt="GitHub icon"
+                  />
+                </a>
+              </Col>
+              <Col className="displayFlex justifyContentCenter" xs={4} xl={3}>
+                <a
+                  href="https://www.instagram.com/a.hayward11/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-responsive icon btnCol"
+                    src={instagramLogo}
+                    alt="Instagram icon"
+                  />
+                </a>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div
+                  id="finalLine"
+                  className="displayFlex justifyContentCenter"
+                >
+                  <div>I look forward to connecting with you</div>
+                </div>
+              </Col>
+            </Row>
+          </div>
         </Col>
       </Row>
     </Container>
